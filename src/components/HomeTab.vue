fcb934ae99429aba2db321ab8ff867088a5eb484
qwertys318 <343604@gmail.com>
2021-07-28 13:26:33 +0700

    redeploy demo debot

--- src/components/HomeTab.vue
+++ src/components/HomeTab.vue
@@ -19,21 +19,9 @@ export default {
     bots: [
       {
         name: 'Interfaces Demo',
-        address: {account: '0:16f2d38778dd31f67854939e95eb30438074ef4c43265d91bc54dfe94a031880', server: 'net.ton.dev'},
+        address: {account: '0:5e4380c442b9cf09c113367e6f4c74b715d3826c57323491d7a1efc1c36c9ce9', server: 'net.ton.dev'},
         color: '#ff7163',
         icon: 'mdi-eye',
-      },
-      {
-        name: 'TIP-3 Manager',
-        address: {account: '0:81c12c2f4514124536aafea59db7df0262d3af877b4477afe6514bbc5bc9f317', server: 'net.ton.dev'},
-        color: '#ed732d',
-        icon: '$vuetify.icons.coins',
-      },
-      {
-        name: 'Demiurge SMV',
-        address: {account: '0:2a92e3d01c530697a0ec3ab5c3494474faa7ea0af5fa30d4ed508115bc9957a7', server: 'net.ton.dev'},
-        color: '#5dcc89',
-        icon: 'mdi-wizard-hat',
       },
     ],
   }),
