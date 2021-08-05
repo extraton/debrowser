e63bbc1217fe8ddc0be80a59f9acd503ccf20e2d
qwertys318 <343604@gmail.com>
2021-08-05 17:05:51 +0700

    build script fix

--- deploy/build.sh
+++ deploy/build.sh
@@ -10,5 +10,5 @@ yarn run build
 docker build -f ../deploy/Dockerfile -t extraton/extraton-debrowser:"${1}" -t docker.pkg.github.com/extraton/debrowser/debrowser-nginx:${1} .
 cd ../
 rm -rf build
-docker push extraton/extraton-debrowser:1.4.0
-docker push docker.pkg.github.com/extraton/debrowser/debrowser-nginx:1.4.0
+docker push extraton/extraton-debrowser:"${1}"
+docker push docker.pkg.github.com/extraton/debrowser/debrowser-nginx:"${1}"
