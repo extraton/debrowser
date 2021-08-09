7530d9058591ee9545ecd4a10796d63629e0bebf
qwertys318 <343604@gmail.com>
2021-08-09 10:32:22 +0700

    interfaces.json

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const CopyPlugin = require('copy-webpack-plugin');
+const JSONMinifyPlugin = require('node-json-minify'),
+  CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   transpileDependencies: [
@@ -13,6 +14,11 @@ module.exports = {
       new CopyPlugin({
         patterns: [
           {from: './node_modules/@tonclient/lib-web/tonclient.wasm', to: 'tonclient_1.13.0.wasm'},
+          {
+            from: './src/lib/browser/interface/interfaces.json', to: 'interfaces.json', transform: function (content) {
+              return JSONMinifyPlugin(content.toString());
+            },
+          },
         ],
       }),
     ],
