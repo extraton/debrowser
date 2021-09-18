61e39f39b01d6d3e2470ae7cd0a059e9efc29fcd
qwertys318 <343604@gmail.com>
2021-09-18 19:06:32 +0700

    docker.pkg.github.com -> ghcr.io

--- deploy/build.sh
+++ deploy/build.sh
@@ -7,8 +7,8 @@ git clone https://github.com/extraton/debrowser.git .
 git checkout "${1}"
 yarn install
 yarn run build
-docker build -f ../deploy/Dockerfile -t extraton/extraton-debrowser:"${1}" -t docker.pkg.github.com/extraton/debrowser/debrowser-nginx:${1} .
+docker build -f ../deploy/Dockerfile -t extraton/extraton-debrowser:"${1}" -t ghcr.io/extraton/debrowser/debrowser-nginx:${1} .
 cd ../
 rm -rf build
 docker push extraton/extraton-debrowser:"${1}"
-docker push docker.pkg.github.com/extraton/debrowser/debrowser-nginx:"${1}"
+docker push ghcr.io/extraton/debrowser/debrowser-nginx:"${1}"
